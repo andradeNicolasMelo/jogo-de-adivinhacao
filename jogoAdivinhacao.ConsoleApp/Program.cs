6c66485f7a92cdfeb855a165efe99189e85344e5
Andrade <andrade.nicolas.melo@outlook.com>
2025-04-05 00:36:20 -0300

    Conclusão do projeto.
    O que foi feito:
    - Realização dos códigos
    - Otimização do layout
    - Testes
    
    Conclusão:
    Jogo esta rodando conforme o objetivo

--- jogoAdivinhacao.ConsoleApp/Program.cs
+++ jogoAdivinhacao.ConsoleApp/Program.cs
@@ -1,10 +1,64 @@
-﻿ namespace jogoAdivinhacao.ConsoleApp
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace jogoAdivinhacao.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            while (true)
+            {
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("Jogo de adivinhação");
+                Console.WriteLine("=================================");
+
+                Random geradorDeNumeros = new Random();
+                int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+                Console.Write("Digite um numero para chutar: ");
+                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                while (numeroDigitado != numeroSecreto)
+                {
+                    if (numeroDigitado < numeroSecreto)
+                    {
+                        Console.WriteLine($"O numero secreto é maior que {numeroDigitado}");
+                        Console.WriteLine("---------------------------------");
+                    }
+
+                    else if (numeroDigitado > numeroSecreto)
+                    {
+                        Console.WriteLine($"O numero secreto é menor que {numeroDigitado}");
+                        Console.WriteLine("---------------------------------");
+                    }
+
+                    Console.Write("Escreva o numero novamente: ");
+                    numeroDigitado = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("Parabens! Você acertou o numero secreto :)");
+                Console.WriteLine($"O numero escrito por você foi {numeroDigitado}, e o numero secreto é {numeroSecreto}");
+                Console.WriteLine("");
+
+                Console.WriteLine("=================================");
+                Console.Write("Quer jogar denovo? ");
+                string sair = Console.ReadLine();
+                Console.WriteLine("=================================");
+
+                if (sair.Contains("s"))
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
